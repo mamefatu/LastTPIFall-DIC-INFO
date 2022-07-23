36732bfb3c31b51a8e7ec28ba3d3176a5c86997b
Adji Fatou <87047826+afmim@users.noreply.github.com>
2022-07-23 08:28:36 +0000

    Update Pile.java

--- ca/quens/sat/master/Pile.java
+++ ca/quens/sat/master/Pile.java
@@ -1,74 +1,42 @@
-package ca.quens.sat.master;
+package ca.queens.sat.master;
+
 
 public class Pile {
-    private int sommet;
-    private int[] maPile;
-    private int tailleMax;
+    private int taille= 10;
+    private int position;
+    int [] p;
 
-    public void setTailleMax(int tailleMax) {
-        if (tailleMax > 0) {
-            this.tailleMax = tailleMax;
+    public Pile (int taille) { 
+        p = new int[taille];
+        position = -1;     
         }
-    }                                                      
 
-    public Pile(int n) {
-        this.setTailleMax(n);
-        this.maPile = new int[tailleMax];
-         this.sommet = -1;
-        
-    }
-    public int getSommet(){
-        return maPile[sommet];
+    public boolean pileVide( ){
+        return position==0;
     }
 
-    public boolean pileVide(Pile p) {
-        if (p.sommet == -1){
-            return true;
-        } else return false;
-        
+    public boolean pilePleine( ){
+        return taille == position;
     }
 
-    public boolean pilePleine(Pile p) {
-        if ((tailleMax) == (sommet ))
-            return true;
-        else return false;
-    }
 
-    public void empiler(Pile p, int element) {
-        if (pilePleine(p)) {
-            throw new Error("La pile est pleine impossible d'empiler");
-        } else {
-            maPile[sommet + 1] = element;
-            sommet++;
+    void empiler(int element){
+        if (pilePleine()){
+            throw new Error("Pile pleine");
         }
-       
-        System.out.println(+ element);
-        
-    }
+        p[position+1] = element;
+        position++;
+      }
 
-   public int depiler(Pile p) {
-    
-        if (pileVide(p)) {
-            throw new Error("La pile est vide impossible de depiler");
-        } else {
-           
-            sommet--;
-            return maPile[sommet + 1];
+      int depile(){
+        if (pileVide()){
+            throw  new  Error("Pile Vide");
         }
-    }
+        position--;  
+        return p[position+1];      
+     }
 
-   public String afficher(Pile p) {
-    int n = sommet;
-    String response = "[";
-    if(pileVide(p)) {
-        return "[]";
-    }
-    while(n >= 0) {
-        response += ""+maPile[n] +", ";   
-        n--;
-    }
-    response += "]";  
-    return response;   
-    }
+   
+
 
 }
